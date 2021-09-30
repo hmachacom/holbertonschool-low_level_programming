01df0878d736e38ae6470c8d0a9bb54c63aef792
Hugo <hamchacom@gmail.com>
2021-09-30 11:59:29 -0700

    Añadiendo cocumentacion al codigo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
+/*
+ *File Edit Options Buffers Tools C Help
+ *Detalles: se inicia la libreria a usar
+ *se usa la funcion main 
+ *Se espera que el programa imprima "Programming is like building a multilingual puzzle"
+ *y retorne 0
+*/
 #include<stdio.h>
 int main (void)
 {
