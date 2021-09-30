f8a0d518e09edd8caba40d5a43b73b4f3944d483
Hugo <hamchacom@gmail.com>
2021-09-30 12:17:15 -0700

    correccion 7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-/*
+/**
  *File Edit Options Buffers Tools C Help
  *Detalles: se inicia la libreria a usar
  *se usa la funcion main
  *El programa imprima "Programming is like building a multilingual puzzle"
  *y retorne 0
-*/
+ */
 
 int main(void)
 {
         puts ("\"Programming is like building a multilingual puzzle");
-        return(0);
+        return (0);
 }
