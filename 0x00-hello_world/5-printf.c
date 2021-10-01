7b0d6881762410a12112f1ffc0f0f613b325ee4f
Hugo <hamchacom@gmail.com>
2021-09-30 19:59:44 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - inicioo
+ *
+ *description: printf with proper grammar, but the outcome is a piece of art,
+ *Retrun:always 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0)
+}
