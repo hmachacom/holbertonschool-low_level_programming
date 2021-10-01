ad1942202591a8b6d9e68d8d8ba7aff60d664741
Hugo <hamchacom@gmail.com>
2021-10-01 15:48:25 -0700

    add style Betty

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-*main:-num 0-9
+*main:-print num 0-9
 *
 *Return: Always 0 (Success)
 */
 int main(void)
 {
 	int num;
+	
 	for (num = 0; num < 10; num++)
 	{
 		printf("%i", num);
