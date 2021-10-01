9a89aca7bf791a852e10d0cf90077f187f3dbe89
Hugo <hamchacom@gmail.com>
2021-10-01 15:43:29 -0700

    5-printe_numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main:-num 0-9
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++;)
+	{
+		printf("%i", num);
+	}
+	printf("\n");
+	return (0);
+}
