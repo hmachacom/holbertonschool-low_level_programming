2e3c4801b0a220b8f013f96cae1194b722528933
Hugo <hamchacom@gmail.com>
2021-10-01 13:45:09 -0700

    2print_alphabt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main:-abecedario
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+    int a = 26;
+    char abc = 'a';
+    while (a != 0)
+    {
+        putchar(abc);
+        abc ++;
+        a --;
+    }
+    return (0);
+}
