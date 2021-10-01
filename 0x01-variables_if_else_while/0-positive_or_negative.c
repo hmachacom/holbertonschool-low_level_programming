1979496f4b4db45b74a023a6f130874b31cc33ea
Hugo <hamchacom@gmail.com>
2021-10-01 11:06:13 -0700

    style Betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	{
 		if (n > 0)
 		{
-			printf("%i is positive\n" ,n);
+			printf("%i is positive\n", n);
 		}
 		else
-			printf("%i is negative\n" ,n);
-	}else
-		printf("%i is zero\n" ,n);
+			printf("%i is negative\n", n);
+	} else
+		printf("%i is zero\n", n);
 	return (0);
 }
