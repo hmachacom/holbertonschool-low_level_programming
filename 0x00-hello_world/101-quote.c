3c737742522e99626811f591ce5c42caa6bf2a05
Hugo <hamchacom@gmail.com>
2021-09-30 21:59:19 -0700

    1001

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+
+/*main- print a text .
+ *
+ *Return: Always 1(Success)
+ */
+int main(void)
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
