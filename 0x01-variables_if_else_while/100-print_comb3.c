42cf52b1bf9d3f78da0df32249c575d26af7c461
Hugo <hamchacom@gmail.com>
2021-10-01 20:49:43 -0700

    100-printcom3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 - 99.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int cont = 0;
+
+	while (cont < 100)
+	{
+		putchar(cont / 10 + '0');
+		putchar(cont % 10 + '0');
+		if (cont != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		cont++;
+	}
+	putchar('\n');
+	return (0);
+}
