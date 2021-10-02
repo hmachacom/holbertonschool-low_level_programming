9f925782ba503caf166ac2413c1b275a29536f6d
Hugo <hamchacom@gmail.com>
2021-10-01 18:27:02 -0700

    6-printNumPutchar

---
+++ 0x01-variables_if_else_while/6-print_numerz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main: - It seeks to print the numbers from 0-9 only using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, s;
+	char num = '1';
+	s = '\n';
+	for (n = 1; n <= 9; n++)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar(s);
+	return (0);
+}
