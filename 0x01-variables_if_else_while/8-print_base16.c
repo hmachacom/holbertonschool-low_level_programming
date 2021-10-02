d46ef103e9f7079af47f03629d0df53e075b7d5e
Hugo <hamchacom@gmail.com>
2021-10-01 19:54:37 -0700

    8print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - print num base 16
+ *
+ *Return: Always 0 (succes)
+ */
+int main(void)
+{
+	int n, s, num = '0';
+	char abc = 'a';
+
+	s = '\n';
+	n = 10;
+	while (n != 0)
+	{
+		putchar(i);
+		num++;
+		n--;
+	}
+	n = 6;
+	while (n != 0)
+	{
+		putchar(abc);
+		n--;
+		abc++;
+	}
+	putchar(s);
+	return (0);
+}
