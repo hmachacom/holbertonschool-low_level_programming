281d41d8ffe195fa1cb0746d93c9983738f77fd1
Hugo <hamchacom@gmail.com>
2021-10-01 17:53:33 -0700

    white space correction and better description of the main function

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-*main:-print num 0-9
+*main:-It seeks to print the numbers from 0 to 9 automatically.
 *
 *Return: Always 0 (Success)
 */
 int main(void)
 {
 	int num;
-	
+
 	for (num = 0; num < 10; num++)
 	{
 		printf("%i", num);
