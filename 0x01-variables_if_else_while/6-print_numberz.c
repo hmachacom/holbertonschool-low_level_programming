56ee654b47710acdb7be293561ed386dc60a56c3
Hugo <hamchacom@gmail.com>
2021-10-01 18:38:05 -0700

    elinando variables char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main: - It seeks to print the numbers from 0-9 only using the putchar function.
+ * main: - It seeks to print the numbers from 0-9 only using theputcharfunction.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n, s, num = '1';
+	
 	s = '\n';
-	for (n = 1; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		putchar(num);
 		num++;
