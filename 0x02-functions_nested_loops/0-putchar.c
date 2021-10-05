a19fbc53d28988b97073a3f92fcf8a547b484b95
Hugo Machacon <hmachacom@gmail.com>
2021-10-05 22:35:06 +0000

    add dir | func print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main:-print "putchar"
+*
+*Return: zero ok.
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
