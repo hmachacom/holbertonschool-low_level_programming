c86295a70d2d6ab1598707b6a095d6454347b1dd
Hugo Machacon <hmachacom@gmail.com>
2021-10-06 23:09:23 +0000

    add 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add:-sumar dos numeros
+*@n:-primernumero
+*@m:-segundonumero
+* Return: zero ok
+*/
+int add(int n, int m)
+{
+	return (n + m);
+}
+
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+*print_last_digit:-function that prints the last digit of a number.
+*@n:-number
+* Return: zero ok
+*/
