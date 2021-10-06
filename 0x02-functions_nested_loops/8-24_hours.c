4b0ac40be0cd662c01726952ae97fc1f8e0e3a6b
Hugo Machacon <hmachacom@gmail.com>
2021-10-06 23:34:01 +0000

    correcion ce variables y ciclos

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,28 @@
 #include "main.h"
 /**
-*print_last_digit:-function that prints the last digit of a number.
-*@n:-number
+*jack_bauer:-imprimir las horas del dia.
+*
 * Return: zero ok
 */
+void jack_bauer(void)
+{
+	int h = 0, m = 0, hh, mm;
+
+	while (h <= 23)
+	{
+		while (m <= 59)
+		{
+			_putchar(h / 10 + '0');
+			hh = h % 10;
+			_putchar(hh + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			mm = m % 10;
+			_putchar(mm + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+		m = 0;
+	}
+}
