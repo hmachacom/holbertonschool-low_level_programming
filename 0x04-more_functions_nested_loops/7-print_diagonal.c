9c3871b6339492b441a6ec576f8fc1d80a5c5d55
Hugo Machacon <hmachacom@gmail.com>
2021-10-08 23:26:43 +0000

    programa que imprim \ n vece

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,15 +6,23 @@
 */
 void print_diagonal(int n)
 {
-	int c = 0;
-
-	if (n > 0)
+if (n <= 0)
 	{
-		while (c < n)
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			_putchar(92);
-			c++;
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar(92);
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
