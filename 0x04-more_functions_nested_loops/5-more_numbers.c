fe58e62c660c3cce82cfb0e3aa10054b25751502
Hugo Machacon <hmachacom@gmail.com>
2021-10-08 22:12:50 +0000

    programa que imprime los numero de 0-14 dies veses

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* more_numbers:-Imprime los numero de 0 al 14 y 14 veces
+*
+* Return: Always 0.
+*/
+void more_numbers(void)
+{
+	int n, c = 0;
+
+	while (c <= 10)
+	{
+		n = 0;
+		while (n <= 14)
+		{
+			if (n > 9)
+				{
+					_putchar((n / 10) + '0');
+				}
+						_putchar((n % 10) + '0');
+			n++;
+		}
+		c++;
+		_putchar('\n');
+	}
+}
