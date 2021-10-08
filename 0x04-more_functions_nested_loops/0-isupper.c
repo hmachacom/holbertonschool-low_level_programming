45b5518c46a18cd0810599b92eb8c47c3f85904d
Hugo Machacon <hmachacom@gmail.com>
2021-10-08 19:15:21 +0000

    programa que detecta si un caracter es mayuscula o no

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper:- check the code.
+ *@c:variable de entrada.
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c) > 0)
+		return (1);
+	else
+		return (0);
+}
