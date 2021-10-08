c8dd319d9891cb367ed5be085d4ecaaec336b6a9
Hugo Machacon <hmachacom@gmail.com>
2021-10-08 20:12:29 +0000

    programa que multiblica a y b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul:-Devuelve la multiplicacion de a y b.
+ *@a:variable de entrada.
+ *@b:variable de entrada.
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
