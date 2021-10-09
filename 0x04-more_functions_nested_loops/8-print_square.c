18fa92c006afc634a1ad3a161034b03a4704a0a2
Hugo Machacon <hmachacom@gmail.com>
2021-10-09 03:00:31 +0000

    add all

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*print_diagonal(int n):-imprimir ("_") n veces;
+*@n:-dato de entrada
+* Return: Always 0.
+*/
+void print_diagonal(int n)
+{
+if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar(92);
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
