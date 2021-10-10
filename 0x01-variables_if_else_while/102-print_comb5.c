9ced6dccda93b2610064c3429bb2a975d15623dd
Hugo Machacon <hmachacom@gmail.com>
2021-10-10 20:14:01 +0000

    add  102-print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,31 +7,31 @@
 
 int main(void)
 {
-    int c1 =0, c2;
+	int c1 = 0, c2 = 1;
 
-    while (c1 < 100)
-    {
-        c2 = 1;
-        while (c2 < 100)
-        {
-            putchar(c1 / 10 + '0');
-            putchar(c1 % 10 + '0');
-            putchar(' ');
-            putchar(c2 / 10 + '0');
-            putchar(c2 % 10 + '0');
-            if ((c1 == 99) && (c2 == 99))
-            {
-                break;
-            }
-            else 
-            {
-                putchar(',');
-                putchar(' ');
-            }
-            c2++;
-        }
-        c1++;
-    }
-    putchar('\n');
-    return (0);
+	while (c1 < 100)
+	{
+		while (c2 < 100)
+		{
+			putchar(c1 / 10 + '0');
+			putchar(c1 % 10 + '0');
+			putchar(' ');
+			putchar(c2 / 10 + '0');
+			putchar(c2 % 10 + '0');
+			if ((c1 == 99) && (c2 == 99))
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			c2++;
+		}
+		c1++;
+		c2++;
+	}
+	putchar('\n');
+	return (0);
 }
