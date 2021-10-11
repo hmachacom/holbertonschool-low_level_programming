821e4f054f7f73050b01f2c049b63643c01347c6
Hugo Machacon <hmachacom@gmail.com>
2021-10-11 22:35:59 +0000

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98:- check the code
+ *@a:-punter int1.
+ *@b:- punter int2.
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
