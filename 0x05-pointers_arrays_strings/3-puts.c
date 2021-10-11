f31f42316e46ea35ccbfaf26a52f8a588ff12066
Hugo Machacon <hmachacom@gmail.com>
2021-10-11 23:23:24 +0000

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts:- check the code
+ *@str:-string.
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
