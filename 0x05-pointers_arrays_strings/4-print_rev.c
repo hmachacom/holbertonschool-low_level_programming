a4f3b0bacf8291fd0c90f60efc6bbc8725496acd
Hugo Machacon <hmachacom@mail.com>
2021-10-12 02:15:37 +0000

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_rev:- check the code
+ *@s:-string.
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	while (l >= 0)
+	{
+		putchar(*s);
+		s--;
+		l--;
+	}
+	putchar('\n');
+}
