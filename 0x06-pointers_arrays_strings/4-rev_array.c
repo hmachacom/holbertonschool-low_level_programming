160de3e7cdc64150b78218b804fa702794c21181
Hugo Machacon <hmachacom@mail.com>
2021-10-14 21:57:13 +0000

    add4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *reverse_array-inverts the matrix of numbers.
+ *@a:array that is reversed.
+ *@n:number of array elements.
+*/
+void reverse_array(int *a, int n)
+{
+	int l, m, j = 0;
+
+	m = n - 1;
+	while (j < n / 2)
+	{
+		l = a[j];
+
+		a[j] = a[m];
+
+		a[m--] = l;
+
+		j++;
+	}
+}
