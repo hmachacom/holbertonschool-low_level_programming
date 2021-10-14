8d844ad0c0546f0a5c9f46d8c2989f78351cd3f4
Hugo Machacon <hmachacom@mail.com>
2021-10-14 21:07:33 +0000

    funcion que copia n numero de byts en otra cadena

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy:- check the code
+ *@dest:string1
+ *@src:string2
+ *@n: numero de byts
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
