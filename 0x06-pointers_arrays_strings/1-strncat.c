d34f85447fac75782dd09a84d66871725d3f8127
Hugo Machacon <hmachacom@mail.com>
2021-10-14 19:43:42 +0000

    add funcion que concatene n numero de byts de otra  string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncat:- check the code
+ *@dest:string1
+ *@src:string2
+ *@n: numero de byts a copiar
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+}
