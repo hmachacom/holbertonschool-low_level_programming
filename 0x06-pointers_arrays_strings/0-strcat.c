046addae378bcad370df985db30a64e589c64ace
Hugo Machacon <hmachacom@mail.com>
2021-10-15 17:31:36 +0000

    correcion de strcopy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat:- check the code
+ * _strcat:- funcion que contatena dos string
  *@dest:string1
  *@src:string2
  * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, src));
+	int i;
+
+	char *e = dest;
+
+	for (i = 0; *dest != '\0'; i++)
+	dest++;
+
+	for (i = 0; *src != '\0'; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	dest = e;
+	return (dest);
 }
