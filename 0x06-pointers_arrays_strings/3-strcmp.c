1395e9ec64c4d76121e9476e425c758bf80469d7
Hugo Machacon <hmachacom@mail.com>
2021-10-16 02:59:50 +0000

    Correccion de 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- *_strcmp:- check the code
+ *_strcmp:- Funcion que compara dos cadenas
  *@s1:strr 2
  *@s2:string2
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(s1, s2));
+	int i;
+/*
+*en el for se valida que ambas cadenas en la posicion
+*i sean iguales si no lo son se sale del ciclo
+*/
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
+	;
+	return (s1[i] - s2[i]);
+
 }
