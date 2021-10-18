3703a7ba29c09868ddb4c17276ddb44c52308dbc
Hugo Machacon <hmachacom@mail.com>
2021-10-18 19:13:31 +0000

    add file 1-mencpy funcion que copia n caractres de un arreglo en otro arreglo

---
+++ 0x07-pointers_arrays_strings/1-mencpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy:- func copy n char the a array
+ * @dest:first position of the array
+ * @src:replacement characters
+ * @n:number of characters to replace
+ * Return: pointer.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; dest[i] = src[i], i++)
+	;
+	return (dest);
+}
