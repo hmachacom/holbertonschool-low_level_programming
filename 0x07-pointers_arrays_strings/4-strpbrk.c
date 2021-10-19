d1d3c6082370e10f2f3b270f5f39fea305bd7cc8
Hugo Machacon <hmachacom@mail.com>
2021-10-19 01:11:47 +0000

    add file 4-strpbrk funt que busca una coincidencia en una cadena

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk:- func scan char the a array
+ * @s:string to scan
+ * @accept:- char a buscar
+ * Return: pointer.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *s != '\0'; s++, i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+	}
+	*s = '\0';
+	return (s);
+}
