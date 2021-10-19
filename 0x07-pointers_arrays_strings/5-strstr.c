3f0586117025bab9017c0a7ef9e1ad76f245537b
Hugo Machacon <hmachacom@mail.com>
2021-10-19 19:09:26 +0000

    add condicional

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,6 +8,9 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j = 0;
+
+	if ((*haystack != '\0') && (*needle == '\0'))
+		return (haystack);
 if ((*haystack != '\0') || (*needle != '\0'))
 {
 	for (i = 0; haystack[i] != '\0'; i++)
