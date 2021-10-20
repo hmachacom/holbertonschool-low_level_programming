ab9c9f2ec45531df20f2fc24d61cfdbf5d672c9d
Hugo Machacon <hmachacom@mail.com>
2021-10-20 22:09:25 +0000

    add file 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string;-print la suma de las diagonales matris cuadrada
+ * @s:puntero a modificar.
+ * @to:;- direccion
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
