17de9f54286e184023b5117c6b108bccc3c56d3f
Hugo Machacon <hmachacom@mail.com>
2021-10-22 17:36:44 +0000

    add file 1-args programa que muestra su propia logitud

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main:-progrma que imprime su nombre seguido de \\n
+ *@argc: lon array
+ *@argv:array
+ * Return: Always 0s
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
