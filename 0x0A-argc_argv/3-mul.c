0f17e261405b7119ee0d2cc3253f579bd25b13bb
Hugo Machacon <hmachacom@mail.com>
2021-10-22 19:21:37 +0000

    add file 3-mull programa que multiplica dos parametro

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,8 +11,10 @@ int main(int argc, char *argv[])
 	int i;
 
 	if (argc != 3)
-		printf("Error");
+	{
+		printf("Error\n");
 		return (1);
+	}
 	i = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", i);
 	return (0);
