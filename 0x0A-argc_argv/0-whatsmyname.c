99f08dc84c8a238173874d5d49600dd41fbec47e
Hugo Machacon <hmachacom@mail.com>
2021-10-22 17:17:29 +0000

    add dir 0x0A, file README, 0-Whatsmyname and main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main:-progrma que imprime su nombre seguido de \\n
+ *@argc: lon array
+ *@argv:array
+ * Return: Always 0s
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
