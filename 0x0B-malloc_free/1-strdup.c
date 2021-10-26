14e6a93003074d37143ddd2378c1b59ffd18a977
Hugo Machacon <hmachacom@mail.com>
2021-10-26 22:32:13 +0000

    funcion que copian un strin y devuelve un puntero a la posicion

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strdup:- which contains a copy of the string given as a parameter.
+ *@str:string imput
+ * Return: pointer.
+ */
+char *_strdup(char *str)
+{
+	int i, m = 0;
+
+	char *migo;
+
+	if (str == NULL)
+	return (NULL);
+	while (str[m] != '\0')
+	{
+		m++;
+	}
+	m--;
+	migo = malloc(sizeof(char) * (m));
+	if (migo == NULL)
+		return (NULL);
+	for (i = 0; i <= m; i++)
+	{
+		migo[i] = str[i];
+	}
+	return (migo);
+}
