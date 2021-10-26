cfebd8744cfb1a6c796f289a003cb9df4a437e3e
Hugo Machacon <hmachacom@mail.com>
2021-10-26 18:09:42 +0000

    correcion condicion null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	m = malloc(sizeof(char) * (size));
+	if (m == NULL)
+		return (NULL);
 	for (i = 0; i <= size; i++)
 	{
 		m[i] = c;
