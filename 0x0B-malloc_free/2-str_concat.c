7aab626d2566c32850ac552dc94e07e26ba4564c
Hugo Machacon <hmachacom@mail.com>
2021-10-27 02:37:28 +0000

    correcion null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	if ((s2 == NULL) && (s1 != NULL))
 		return (s1);
 	if ((s2 == NULL) && (s1 == NULL))
-		return (s1);
+		return (NULL);
 	while (s1[m] != '\0')
 	{
 		m++;
