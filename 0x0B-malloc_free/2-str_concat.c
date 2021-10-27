49069695690eec2910ae66e6997383ce33f733b0
Hugo Machacon <hmachacom@mail.com>
2021-10-27 16:52:07 +0000

    cabio radical de 2-str_concat correccion de betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,38 +7,37 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, m = 0;
+	int i = 0, m = 0, l = 0, o = 0;
 	char *migo;
 
-	if ((s2 == NULL) && (s1 == NULL))
-	{
-		migo = malloc(sizeof(char) * (2));
-		return (migo);
-	}
-	if ((s2 == NULL) && (s1 != NULL))
+	if (((s2 == NULL) || (*s2 == '\0')) && s1 != NULL)
 	{
 		for (m = 0; s1[m] != '\0'; m++)
 			;
 		migo = malloc(sizeof(char) * (m + 1));
-		for (i = 0; s1[i] != '\0'; migo[i] = s1[i], i++)
+		for (l = 0; s1[l] != '\0'; migo[l] = s1[l], l++)
 			;
 		return (migo);
 	}
-	if ((s1 == NULL) && (s2 != NULL))
+	if (((s1 == NULL) || (*s1 == '\0')) && s2 != NULL)
 	{
 		for (i = 0; s2[i] != '\0'; i++)
 			;
 		migo = malloc(sizeof(char) * (i + 1));
-		for (m = 0; s2[m] != '\0'; migo[m] = s2[m], m++)
+		for (o = 0; o < i; migo[o] = s2[o], o++)
 			;
+		return (migo);
+	}
+	if ((s2 == NULL) && (s1 == NULL))
+	{
+		migo = malloc(sizeof(char) * (2));
 		return (migo);
 	}
 	for (m = 0; s1[m] != '\0'; m++)
 		;
 	for (i = 0; s2[i] != '\0'; i++)
 		;
-	m += i;
-	migo = malloc(sizeof(char) * (m + 1));
+	migo = malloc(sizeof(char) * (m + i + 1));
 	if (migo == NULL)
 		return (NULL);
 	for (m = 0; s1[m] != '\0'; migo[m] = s1[m], m++)
