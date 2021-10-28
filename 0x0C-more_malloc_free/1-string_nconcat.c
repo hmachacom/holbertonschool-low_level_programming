3c5efa78a5773c09edf1cafeecc2b434cd1607ff
Hugo Machacon <hmachacom@mail.com>
2021-10-28 22:42:23 +0000

    funcion que contatena dos cadenas pero solo copia los n bytes solicitados de la segunda cadena 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,12 +13,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *h;
 
 	if (s1 == NULL)
-		i = 1;
+		i = 0;
 	else
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	if (s2 == NULL)
-		m = 1;
+		m = 0;
 	else
 	for (m = 0; s2[m] != '\0'; m++)
 		;
