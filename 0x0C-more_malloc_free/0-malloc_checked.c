928991a0c9184056481a143c83d6228f315d8af0
Hugo Machacon <hmachacom@mail.com>
2021-10-28 16:35:35 +0000

    funcion que reserva un estacion especifico en la memoria dinamica

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked:- reserva espacion en memoria dinamica
+ * @b:catidad de memoria a reservar
+ *
+ * Return: Nothing.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *c;
+	c = malloc(b);
+	if (c == NULL)
+	{
+		exit(98);
+	}
+	return (c);
+}
