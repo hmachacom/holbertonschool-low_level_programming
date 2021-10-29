f73dda63228f9b5a71d19c8b525c5a63c3f49e6c
Hugo Machacon <hmachacom@mail.com>
2021-10-29 18:33:06 +0000

    funcion que crea una matrix desde min hasta max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * array_range:- funcion que crea una matrix desde min hasta max
+ *@min:-valor minimo de la matriz
+ *@max:-valor maximo de la matriz
+ * Return: pointer.
+ */
+int *array_range(int min, int max)
+{
+	int *matrix, i;
+
+	if (min > max)
+		return (NULL);
+	matrix = malloc(sizeof(int) * (max - min) + 1);
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i <= max; i++)
+	{
+		*(matrix + i) = min + i;
+	}
+	return (matrix);
+}
