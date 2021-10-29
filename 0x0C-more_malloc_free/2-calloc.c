b1be100535642f1d65424e9806e0c91ef15826d8
Hugo Machacon <hmachacom@mail.com>
2021-10-29 17:20:03 +0000

    funcion que reserva n bytes de memoria dinamica para una matriz de tamaño size

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _calloc:- reserva espacion en memoria dinamica
+ *@nmemb:-n nytes a reservar
+ *@size:-tamaño de la matriz
+ * Return: pointer.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *b;
+
+	if ((nmemb <= 0) || (size <= 0))
+		return (NULL);
+	b = malloc(nmemb);
+	if (b == NULL)
+		return (NULL);
+	return (b);
+}
