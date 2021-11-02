edb64b20e5b0bb9addfc901fbae1f3c94f228bdf
Hugo Machacon <hmachacom@mail.com>
2021-11-02 02:08:25 +0000

    correccion de typedefi

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,20 +12,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 #include <stdlib.h>
 #include <stdio.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- *struct dog-new structura
- *@name:-nombre
- *@age:-edad
- *@owner:-Dueño
- */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+
 #endif
