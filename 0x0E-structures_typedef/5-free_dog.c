31fad6857a325b8f706509fa1620d3e6e95882d0
Hugo Machacon <hmachacom@mail.com>
2021-11-02 03:58:18 +0000

    correcion de betty

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,12 +1,12 @@
 #include "dog.h"
 /**
- *new_dog:-nuevo perro
+ *free_dog:-nuevo perro
  *@d:free
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-	free(d->name), free(d->owner), free(d);
+		free(d->name), free(d->owner), free(d);
 	}
 }
