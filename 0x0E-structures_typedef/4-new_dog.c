51febf85af37984c8a2db952e010629de94cefc9
Hugo Machacon <hmachacom@mail.com>
2021-11-02 03:48:47 +0000

    creando copias y funcion para recorrer

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,18 @@
 #include "dog.h"
 /**
+ *lon:-nuevo perro
+ *@mall:new type
+ *Return:-nuevo
+ */
+int lon(char *mall)
+{
+	int c;
+
+	for (c = 0; mall[c] != '\0'; c++)
+		;
+	return (c);
+}
+/**
  *new_dog:-nuevo perro
  *@name:new type
  *@age:age
@@ -10,14 +23,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nuevo;
 	char *namec, *ownerc;
+	int i;
 
 	nuevo = malloc(sizeof(dog_t));
 	if (nuevo == NULL)
 		return (NULL);
+	if (name == NULL || owner == NULL)
+	{
+		free(nuevo);
+		return (NULL);
+	}
+	namec = malloc(sizeof(char) * lon(name) + 1);
+	ownerc = malloc(sizeof(char) * lon(owner) + 1);
 	nuevo->name = name;
 	nuevo->age = age;
 	nuevo->owner = owner;
-	namec = name;
-	ownerc = owner;
+	for (i = 0; name[i] != '\0'; namec[i] = name[i], i++)
+		;
+	namec[i] = '\0';
+	for (i = 0; owner[i] != '\0'; ownerc[i] = owner[i], i++)
+		;
+	ownerc[i] = '\0';
 	return (nuevo);
 }
