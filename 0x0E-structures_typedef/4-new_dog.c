5b7a83cc5ef143746cb8b42aaec8b29186c6d3e5
Hugo Machacon <hmachacom@mail.com>
2021-11-02 02:24:09 +0000

    creando una nueva estructura apartir de una ya existente

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+ *new_dog:-nuevo perro
+ *@name:new type
+ *@age:age
+ *@owner:owner
+ *Return:-nuevo
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *nuevo;
+
+	nuevo = malloc(sizeof(dog_t));
+	if (nuevo == NULL)
+		return (NULL);
+	nuevo->name = name;
+	nuevo->age = age;
+	nuevo->owner = owner;
+	return (nuevo);
+}
