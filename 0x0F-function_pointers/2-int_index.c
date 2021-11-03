f6978c36abff05dcd8f7cd93c65758691d1d239d
Hugo Machacon <hmachacom@mail.com>
2021-11-03 20:28:25 +0000

    función que busque un número entero en un array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ *int_index:-funcion que ejecuta otra funcion en cada elemento del arreglo
+ *@array:-array
+ *@size:-tamaño del array
+ *@cmp:funcion a llamar
+ *Return:- el indici del array si conincide -1 en caso de error
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, j;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		j = (*cmp)(array[i]);
+		if (j != 0)
+			break;
+		if (i == size - 1)
+			return (-1);
+	}
+
+	return (i);
+}
