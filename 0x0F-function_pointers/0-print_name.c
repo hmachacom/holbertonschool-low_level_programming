4de4c43ebe380bf6bb0a62b14c60115fb9e223d0
Hugo Machacon <hmachacom@mail.com>
2021-11-03 17:25:55 +0000

    add files and funcion que llama a otra funcion por medio de un puntero

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ *print_name:-funcion que llama a otra funcion por un puntero
+ *@name:-nombre
+ *@f:-ni idea
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
