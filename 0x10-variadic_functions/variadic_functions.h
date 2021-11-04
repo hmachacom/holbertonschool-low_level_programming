a0f06c5e700e632f61b66c3567cae21bd29c47d3
Hugo Machacon <hmachacom@mail.com>
2021-11-04 18:01:48 +0000

    add prototipo

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #include <string.h>
 #include <stdlib.h>
 #endif
