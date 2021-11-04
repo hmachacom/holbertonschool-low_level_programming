6724079a98c1bd750691e1ccb70c0c55cc4613e0
Hugo Machacon <hmachacom@mail.com>
2021-11-04 22:07:25 +0000

    add

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+/**
+ *print_all:-funcion que imprime n de cadenas separados por un caracter
+ *@format:tipo de dato
+ */
+void print_all(const char *const format, ...)
+{
+	int i = 0;
+	char *p;
+	va_list h;
+
+	va_start(h, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+				printf("%c", va_arg(h, int));
+				break;
+		case 'i':
+				printf("%d", va_arg(h, int));
+				break;
+		case 'f':
+				printf("%f", va_arg(h, double));
+				break;
+		case 's':
+			p = va_arg(h, char *);
+			p ? printf("%s", p) : printf("(nil)");
+			break;
+		default:
+			i++;
+			continue;
+		}
+		i++;
+		if (format[i] != '\0')
+			printf(", ");
+	}
+	printf("\n");
+}
