1e4fa1bc74ccac13b3ab0ebac2ec0b24615f6050
hmachacon <hmachacom@gmail.com>
2021-11-16 21:05:03 +0000

    add prot _putchar

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
- *print_listint:-function print linked list
- *@h: linked list
- * Return: long linked list
+ * print_listint - print linked singly list
+ * @h: list type listint_t
+ * Return: nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t ret = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
+		ret++;
 		h = h->next;
 	}
-	return (count);
+	return (ret);
 }
