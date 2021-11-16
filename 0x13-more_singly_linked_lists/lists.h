1437acc8b0b34dd5b944c5a43c9f3b2062b44b6d
hmachacon <hmachacom@gmail.com>
2021-11-16 21:08:08 +0000

    ada

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,23 +1,22 @@
-#ifndef LINKED_LIST
-#define LINKED_LIST
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- *
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -30,6 +29,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
 
 #endif
