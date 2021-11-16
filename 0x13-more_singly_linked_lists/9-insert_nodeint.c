ff863d1cbd5d528a7fc32d848fe5f22452b8c8ad
hmachacon <hmachacom@gmail.com>
2021-11-16 20:22:58 +0000

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnodo = malloc(sizeof(listint_t));
 	if (!newnodo)
 		return (NULL);
-	if (!*head)
+	if (!*head || !head)
 	{
 		*head = newnodo;
 		newnodo->n = n;
