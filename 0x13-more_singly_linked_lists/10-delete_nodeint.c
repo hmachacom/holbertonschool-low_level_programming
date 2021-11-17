d59637390feb56ec09fb126ce512d977e5229916
hmachacon <hmachacom@gmail.com>
2021-11-17 16:43:00 +0000

    add if

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,10 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (count < index)
 		return (-1);
 	if (!tmp)
-	{
-		free(tmp);
 		return (-1);
-	}
 	tmp2 = tmp->next;
 	tmp->next = tmp2->next;
 	free(tmp2);
