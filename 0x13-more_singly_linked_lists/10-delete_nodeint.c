603eb6079acca6344f30c9fa0ff3f92c159aaf94
hmachacon <hmachacom@gmail.com>
2021-11-17 16:44:50 +0000

    add if

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		tmp = tmp->next;
 		count++;
 	}
-	if (count < index)
+	if (count <= index)
 		return (-1);
 	if (!tmp)
 		return (-1);
