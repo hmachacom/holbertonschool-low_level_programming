23b491a935f96c401090e7d6e1ae22198aa7d78b
hmachacon <hmachacom@gmail.com>
2021-11-23 14:54:21 +0000

    calidacion

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,12 +7,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, c;
+	int o, w = 0, c;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 	o = open(filename, 2 | O_CREAT | O_TRUNC, 0600);
-	w = write(o, text_content, strlen(text_content));
+	if (text_content)
+		w = write(o, text_content, strlen(text_content));
 	c = close(o);
 	if (o == -1 || c == -1 || w == -1)
 		return (-1);
