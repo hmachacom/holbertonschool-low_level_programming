8cc3b58f9e140922384ae44da40dc97f8221ad7c
hmachacon <hmachacom@gmail.com>
2021-11-23 16:29:48 +0000

    condition correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,38 +8,41 @@
  */
 int main(int argc, char *argv[])
 {
-	int file1, file2, close1, close2;
+	int file1, file2, c1, c2, w;
 	char *buf;
-	ssize_t n_bytes = 0;
+	ssize_t o = 0;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buf = malloc(sizeof(argv[1]) * 1024);
+	buf = malloc(sizeof(char) * 1024);
 	if (!buf)
 		return (-1);
 	file1 = open(argv[1], O_RDONLY);
-	file2 = open(argv[2], 2 | O_CREAT | O_TRUNC, 0664);
-	if (file1 == -1)
+	o = read(file1, buf, 1024);
+	if (file1 == -1 || o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file2 == -1)
+	file2 = open(argv[2], 2 | O_CREAT | O_TRUNC, 0664);
+	w = write(file2, buf, o);
+	if (file2 == -1 || w == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(98);
+		exit(99);
 	}
-	n_bytes = read(file1, buf, 1024);
-	write(file2, buf, n_bytes);
-	close1 = close(file2);
-	close2 = close(file1);
-	if (close1 == -1 || close2 == -1)
+	c1 = close(file2);
+	c2 = close(file1);
+	if (c1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", -1);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c1), exit(100);
+	}
+	if (c2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c2), exit(100);
 	}
 	free(buf);
 	return (0);
