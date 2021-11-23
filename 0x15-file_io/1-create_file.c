4b5373234d9413d05294325a11bb7572c0c14419
hmachacon <hmachacom@gmail.com>
2021-11-23 11:41:34 +0000

    add validacion

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	o = open(filename, 2 | O_CREAT, 0600);
 	if (text_content)
-		w = write(o, text_content, strlen(text_content));
+	{
+		w = write(o, text_content, strlen(text_content) + 1);
+		if (w == -1)
+			return (-1);
+	}
 	c = close(o);
-	if (o == -1 || c == -1 || w == -1)
+	if (o == -1 || c == -1)
 		return (-1);
 	return (1);
 }
