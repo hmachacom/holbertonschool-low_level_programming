a5e595d275746455260207a5f1b6698623d680e0
hmachacon <hmachacom@gmail.com>
2021-11-23 14:51:56 +0000

    add STDOUT

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,15 +11,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename || !text_content)
 		return (-1);
-	o = open(filename, 2 | O_CREAT, 0600);
-	if (text_content)
-	{
-		w = write(STDOUT_FILENO, text_content, strlen(text_content) + 1);
-		if (w == -1)
-			return (-1);
-	}
+	o = open(filename, 2 | O_CREAT | O_TRUNC, 0600);
+	w = write(o, text_content, strlen(text_content));
 	c = close(o);
-	if (o == -1 || c == -1)
+	if (o == -1 || c == -1 || w == -1)
 		return (-1);
 	return (1);
 }
