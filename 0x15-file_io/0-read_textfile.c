9a3f9c0e3950e74294d703c3adf16acafb8d9bc2
hmachacon <hmachacom@gmail.com>
2021-11-23 11:09:32 +0000

    asd

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,8 +15,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	o = open(filename, O_RDONLY);
 	r = read(o, buf, letters);
-	w = write(0, buf, r);
+	w = write(stdout, buf, r);
 	c = close(o);
+	if (r < (ssize_t)letters)
+		return (0);
 	if (o == -1 || r == -1 || w == -1 || c == -1)
 		return (0);
 	free(buf);
