fbc57198b0a1c3a8e6d67542ab9cf42b975868b5
hmachacon <hmachacom@gmail.com>
2022-01-06 20:39:33 +0000

    add file 4-free

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,14 +8,13 @@ void free_dlistint(dlistint_t *head)
 {
 	if (head)
 	{
-		dlistint_t *tmp = head;
+		dlistint_t *tmp;
 
-		while (tmp->next)
+		while (head)
 		{
+			tmp = head->next;
 			free(head);
 			head = tmp;
-			tmp = tmp->next;
 		}
-		free(tmp);
 	}
 }
