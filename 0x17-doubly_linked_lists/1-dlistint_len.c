6303c2d60e9d503b3575af531d3b882b0656cdad
hmachacon <hmachacom@gmail.com>
2022-01-06 18:54:38 +0000

    add

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 /**
- *print_dlistint:-function print doublylinked list
- *@h: linked list
+ *dlistint_len:-function leng doublylinked list
+ *@h: long list
  * Return: long linked list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t elements = 0;
+
 	if (h)
 	{
-		int elements;
-		for(elements = 0; h ; elements++)
+		for (elements = 0; h ; elements++)
 			h = h->next;
-		return(elements);
+		return (elements);
 	}
-	return(0);
+	return (elements);
 }
