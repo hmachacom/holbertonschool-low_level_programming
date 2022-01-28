c3bfa9ad4e3a681d6c01e7a59fed3a5f63f37a96
Hugo Machacon <hmachacom@gmail.com>
2022-01-28 13:01:16 -0500

    add pointer

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!has)
 		return (NULL);
 	has->size = size;
-	has->array = malloc(sizeof(hash_node_t) * size);
+	has->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (has->array == NULL)
 	{
