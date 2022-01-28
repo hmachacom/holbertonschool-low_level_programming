53369e2275effb0aec684ddb90f582ff3b9594b4
Hugo Machacon <hmachacom@gmail.com>
2022-01-28 09:36:52 -0500

    aad val

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *has;
 	unsigned long int index;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
 	has = malloc(sizeof(hash_table_t));
