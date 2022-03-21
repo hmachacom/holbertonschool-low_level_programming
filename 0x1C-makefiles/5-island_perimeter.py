1bbbbc34f9f71d2e4527c4786add4fa6d68fee30
Hugo Machacon <hmachacom@gmail.com>
2022-03-21 17:29:39 -0500

    ad

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,54 +1,21 @@
 #!/usr/bin/python3
 """
 grid es una lista de lista de enteros
-0 representa una zona de agua
-1 representa una zona terrestre
-Una celda es un cuadrado con lado de longitud 1
-Las celdas de la cuadrícula están conectadas horizontal/verticalmente (no en diagonal).
-La cuadrícula es rectangular, el ancho y la altura no superan los 100
-Grid está completamente rodeada de agua y hay una isla (o nada).
-La isla no tiene “lagos” (agua adentro que no está conectada con el agua alrededor de la isla).
 """
 
 
 def island_perimeter(grid):
-    """
-    that returns the perimeter of the island
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    Args:
-        grid (list): is a list of list of integers
-    """
-    assert len(grid) != 0, "error"
-    is_square(grid)
-    print(this_list(grid))
-
-
-def this_list(grid):
-    """crea una nueva lista
-
-    Args:
-        grid (list): none
-    """
-    new_list = []
-    for x in grid:
-        for j in x:
-            if j == 1:
-                new_list.append(j)
-    return new_list
-
-def is_square(grid):
-    """validamos el square
-
-    Args:
-        grid (list): isla
-    """
-    square = len(grid[0])
-    for isla in grid:
-        """
-        validamos que todas que el contenido de grid sea correcto
-        """
-        square2 = len(isla)
-        assert len(isla) != 0, "error"
-        assert square == square2, "Error"
-    
-    
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    edges += 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    edges += 1
+    return size * 4 - edges * 2
